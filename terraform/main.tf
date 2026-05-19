76214bb692a6ac9644a340479d46861d5025865c
Solomon Yaakobov <solomonyaa@gmail.com>
2026-05-19 18:29:33 +0300

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,7 +80,20 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
   eks_managed_node_groups = {
     default = {
